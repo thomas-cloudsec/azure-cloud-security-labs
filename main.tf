e166b1ab1e890d3b137504f1274c74ea07520c02
pegasus-cloudsec <thomasjpeg@gmail.com>
2026-06-16 21:27:49 +0100

    Feat: Add initial Terraform provider configuration and gitignore

---
+++ main.tf
@@ -0,0 +1,21 @@
+# 1. Define the Required Providers and Versions
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# 2. Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# 3. Create the Lab Resource Group (Observation: The Foundational 
+Container)
+resource "azurerm_resource_group" "lab_rg" {
+  name     = "rg-lab-secret-sweep"
+  location = "northeurope" # Set to North Europe (Ireland data center)
+}
