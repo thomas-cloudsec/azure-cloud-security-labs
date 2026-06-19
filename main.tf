69a7f30e8b2719a5bda38bad4e9392ae9403c75d
thomas-cloudsec <thomasjpeg@gmail.com>
2026-06-19 21:02:53 +0000

    Lab 1 Complete: Provisioned VNet, Subnet, and NSG infrastructure foundation

--- main.tf
+++ main.tf
@@ -20,3 +20,39 @@ resource "azurerm_resource_group" "lab_rg" {
   name     = "rg-lab-secret-sweep"
   location = "northeurope" # Set to North Europe (Ireland data center)
 }
+
+# 4. Create a Virtual Network
+resource "azurerm_virtual_network" "lab_vnet" {
+    name                = "vnet-security-lab"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.lab_rg.location
+    resource_group_name = azurerm_resource_group.lab_rg.name
+}
+
+# 5. Create a Subnet for the Honeypot VM
+resource "azurerm_subnet" "lab_subnet" {
+    name                    = "sub-honeypot"
+    resource_group_name     = azurerm_resource_group.lab_rg.name
+    virtual_network_name    = azurerm_virtual_network.lab_vnet.name
+    address_prefixes        = ["10.0.1.0/24"]
+}
+
+# 6. Creating a Network Security Grup (Firewall)
+resource "azurerm_network_security_group" "lab-nsg" {
+    name                    = "nsg-honeypot"
+    location                = azurerm_resource_group.lab_rg.location
+    resource_group_name     = azurerm_resource_group.lab_rg.name
+
+# RULE to allow SSH from the internet for testing/attack simulation
+security_rule {
+    name                        = "Allow-SSH-Inbound"
+    priority                    = 1000
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+ }
+}
